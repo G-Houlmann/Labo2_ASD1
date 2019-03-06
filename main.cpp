a0ee9c8900d2362bb0dc7e8df11f5a159b38a343
BecaudA <becaud.arthur@gmail.com>
2019-03-06 16:16:54 +0100

    Basic main file

--- main.cpp
+++ main.cpp
@@ -1,1 +1,24 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo_2
+ Fichier     : main.cpp/h
+ Auteur(s)   : Arthur Bécaud
+ Date        : 06.03.2019
 
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+
+
+   return EXIT_SUCCESS;
+}
