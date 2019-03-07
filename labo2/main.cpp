31d6fc0f57d3cdf51da5bf98537b5ee3f0ad9211
G-Houlmann <gildas.houlmann@gmail.com>
2019-03-07 10:59:58 +0100

    créé projet vs

---
+++ labo2/main.cpp
@@ -0,0 +1,25 @@
+/*
+ -----------------------------------------------------------------------------------
+ Laboratoire : Labo_2_Puzzle_Impossible
+ Fichier     : main.cpp
+ Auteur(s)   : Arthur Bécaud, Loic Geinoz, Gildas Houlmann
+ Date        : 06.03.2019
+
+ But         :
+
+ Remarque(s) :
+
+ Compilateur : MinGW-g++ 6.3.0
+ -----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include "pieces.h"
+
+using namespace std;
+
+int main() {
+
+
+
+   return EXIT_SUCCESS;
+}
